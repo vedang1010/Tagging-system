cee26c8ffff6d0039ea068b703a60ae4b63672f2
Manthan Kshetrapal <manthans.kshetrapal@gmail.com>
2024-05-23 13:33:05 +0530

    folder structure

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+
+app.use(cors());
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server started at port number ${PORT}`);
+})
+
+app.get("/", (req, res) => {
+    res.send("<h1> Server home page </h1>")
+})
