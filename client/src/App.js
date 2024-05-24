02e0c233c6c94d866d0a513e3221b81d58be14c8
Vedang <vedangkhedekar07@gmail.com>
2024-05-24 19:37:51 +0530

    Simple Template for Admin and Profile pages made

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
+// App.js
+import React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Admin from './Admin';
+import Profile from './Profile';
 function App() {
   return (
-    <div className="App">
-      <h1>This is client home page</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>This is client home page</h1>
+        <Link to="/">
+          <button>Go to Home Page</button>
+        </Link>
+        <Link to="/Admin">
+          <button>Go to Admin Page</button>
+        </Link>
+        <Link to="/Profile">
+          <button>Go to Profile Page</button>
+        </Link>
+        <Routes>
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
