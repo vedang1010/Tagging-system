201fdf2b055bc9cd15c1f249c89bed2c4b8d6df2
Rashi Sinha <rashis1146@gmail.com>
2024-06-10 11:03:55 +0530

    All basic of review done

--- server/routes/review.js
+++ server/routes/review.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const{getAllIdeas, fetchIdea, fetchComponent}= require('../controllers/reviewController');
+const{getAllIdeas, fetchIdea, getAllComponents, updateStatus1, updateStatus2}= require('../controllers/reviewController');
 
 console.log("Welcome1");
 router.get('/getAllIdeas',getAllIdeas);
-router.post('/fetchComponent',fetchComponent);
+router.get('/getAllComponents',getAllComponents);
 router.get('/fetchIdea/:id', fetchIdea);
-
+router.post('/status1',updateStatus1);
+router.post('/status2',updateStatus2);
 module.exports = router;
 
 
