96ec0bb7af478708cce12397fc9fdd2d57d1f824
Rashi Sinha <rashis1146@gmail.com>
2024-06-12 11:36:04 +0530

    error handling

--- client/src/pages/ReviewComponentPage.js
+++ client/src/pages/ReviewComponentPage.js
@@ -48,12 +48,26 @@ useEffect(() => {
   fetchIdeas();
 }, []); 
 
+useEffect(() => {
+  if (error) {
+    Swal.fire({
+      title: "Error",
+      text: error.message,
+      icon: "error",
+    });
+  }
+}, [error]);
+
 if (loading) {
   return <div>Loading...</div>;
 }
 
 if (error) {
-  return <div>Error: {error.message}</div>;
+  return <div>{Swal.fire({
+    title: "Oops!",
+    text: error.message,
+    icon: "error",
+  })}</div>;
 }
 
   return (
