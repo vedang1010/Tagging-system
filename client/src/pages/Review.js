a07a78ddfc6895adb47845595f3cd2b26a6687c4
vedang khedekar <vedangkhedekar07@gmail.com>
2024-07-04 00:37:11 +0530

    made review idea page responsive and improved ui of review 1 and review2

--- client/src/pages/Review.js
+++ client/src/pages/Review.js
@@ -1,80 +1,55 @@
-import React,  { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../styles/Review.module.css';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Swal from "sweetalert2";
-import { useNavigate } from 'react-router-dom';
-import HtmlRenderer from "../utils/HtmlRenderer"
+import HtmlRenderer from "../utils/HtmlRenderer";
 import { RiArrowGoBackFill } from "react-icons/ri";
-
-
+import {
+  Box,
+  IconButton,
+  Typography,
+  Paper,
+  Grid,
+  Button,
+  TextField,
+  Divider,
+  Rating,
+} from '@mui/material';
 
 const Review = () => {
-  const {objectId, reviewId} = useParams();
-  sessionStorage.setItem("location",`/review1/${objectId}/${reviewId}`)
+  const { objectId, reviewId } = useParams();
+  sessionStorage.setItem("location", `/review1/${objectId}/${reviewId}`);
 
   const [rating, setRating] = useState(0);
   const [remarks, setRemarks] = useState('');
-  const [page, setPage] = useState('review'); 
+  const [page, setPage] = useState('review');
   const [ideas, setIdeas] = useState(null);
   const [loading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [contri, setContri] = useState([]);
-  const [status, setStatus] = useState('Pending')
-  const [tech, setTech] = useState('false');
-  const userEmail = localStorage.getItem('user');
+  const [status, setStatus] = useState('Pending');
   const [submitted, setSubmitted] = useState(false);
-  const [hasSubmitted, setHasSubmitted] = useState(false);
   const [errors, setErrors] = useState({ rating: '', remarks: '' });
   const navigate = useNavigate();
-  try{
-    useEffect(() => {
-        console.log("object "+objectId);
-        axios.get("http://127.0.0.1:5000/api/review/fetchIdea/" + objectId).then(response =>{
-            console.log(response.data);
-            const idea = response.data.component  
-            setIdeas(idea);
-            setIsLoading(false);
-            setError(false);
-            console.log('User ID:', userEmail);
-       })
-      .catch((error)=>{
-        console.log("Here is the error ",error);
+  const userEmail = localStorage.getItem('user');
+
+  useEffect(() => {
+    axios.get(`http://127.0.0.1:5000/api/review/fetchIdea/${objectId}`)
+      .then(response => {
+        setIdeas(response.data.component);
+        setIsLoading(false);
+      })
+      .catch(error => {
         setIsLoading(false);
         setError(error);
-      })
-     },[objectId])
-  } catch (error) {
-    console.error(error.message+ " over here ");
-  }
-
-
-  // try{
-  //   useEffect(()=>{
-  //     console.log("user _id : " + userEmail)
-  //     axios.get("http://127.0.0.1:5000/api/review/fetchUserInfo/" + userEmail).then(
-  //       response =>{
-  //         const data = JSON.stringify(response.data);
-  //         console.log(data);
-  //         if(data.subgroup == 2) setTech(true);
-  //         else setTech(false);
-  //       }
-  //     ).catch((error) => {
-  //       console.log("Some error happened")
-  //       console.log(error);
-  //     })
-  //   },[])
-  // }catch (error) {
-  //   console.error(error.message+ " over here 2");
-    
-  // }
+      });
+  }, [objectId]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (page === 'review') {
       setPage('ratings');
     } else {
-      console.log('Form submitted', { rating, remarks });
       setPage('review');
     }
   };
@@ -84,87 +59,50 @@ const Review = () => {
   };
 
   const handleReject = () => {
-    console.log('Rejected with remarks', { remarks, rating });
     setPage('ratings');
     setStatus('Rejected');
-    
   };
 
-  const handleAccept =() =>{
-    console.log('Rejected with remarks', { remarks, rating });
+  const handleAccept = () => {
     setPage('ratings');
     setStatus('Accepted');
-  }
+  };
 
-  const handleOnClick = async()=>{
+  const handleOnClick = async () => {
     setPage('ratings');
     try {
       const response = await axios.post("http://127.0.0.1:5000/api/review/status1", {
-        status: status,
-        remarks: remarks,
-        rating: rating,
-        objectId: objectId,
-        reviewId: reviewId,
+        status,
+        remarks,
+        rating,
+        objectId,
+        reviewId,
       });
-      
-      if (response.status !== 200) {
-        console.log(response.status);
 
-      } else {
+      if (response.status === 200) {
         setSubmitted(true);
+      } else {
         console.log(response.status);
-        
       }
 
       let formIsValid = true;
-    let newErrors = { rating: '', remarks: '' };
+      let newErrors = { rating: '', remarks: '' };
 
-    if (rating === 0) {
-      newErrors.rating = 'Rating is required';
-      formIsValid = false;
-    }
-    if (remarks.trim() === '') {
-      newErrors.remarks = 'Remarks are required';
-      formIsValid = false;
-    }
+      if (rating === 0) {
+        newErrors.rating = 'Rating is required';
+        formIsValid = false;
+      }
+      if (remarks.trim() === '') {
+        newErrors.remarks = 'Remarks are required';
+        formIsValid = false;
+      }
 
-    setErrors(newErrors);
-
-      
+      setErrors(newErrors);
 
     } catch (error) {
       console.error("Error occurred while sending the request:", error);
-      
-    }
-    
-  }
-
-  const handleDownloadAll = (files) => {
-    if (files && files.length > 0) {
-      files.forEach((fileUrl) => {
-        const link = document.createElement('a');
-        link.href = fileUrl;
-        link.setAttribute('download', '');
-        link.setAttribute('target', '_blank'); // Open in a new tab
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      });
-    } else {
-      console.error('No files available for download');
     }
   };
-  // const ideas = {
-  //   name: 'Example Component',
-  //   type: 'UI Element',
-  //   details: 'This is a detailed description of the component.',
-  //   tags: 'UI, Design, Example',
-  //   language: 'JavaScript',
-  //   version: '1.0.0',
-  //   dependencies: 'React, PropTypes',
-  //   input: 'Props',
-  //   output: 'UI Component'
-  // };
 
   useEffect(() => {
     if (error) {
@@ -174,143 +112,137 @@ const Review = () => {
         icon: "error",
       });
     }
-
   }, [error]);
 
   useEffect(() => {
-    if(submitted){
-    Swal.fire({
-    //position: "top-end",
-    icon: "success",
-    title: "Your work has been saved",
-    
-   
-  });
-  navigate('/reviewidea');
+    if (submitted) {
+      Swal.fire({
+        icon: "success",
+        title: "Your work has been saved",
+      });
+      navigate('/reviewidea');
     }
-  },[submitted]);
-  
+  }, [submitted]);
+
   if (loading) {
     return <div>Loading...</div>;
   }
 
   if (error) {
-    return <div>error.msg</div>;
+    return <div>{error.message}</div>;
   }
 
   if (!ideas) {
     return <div>No idea found</div>;
   }
 
- 
   return (
-    <div className={styles.formContainer}>
-    <RiArrowGoBackFill onClick={() => navigate(-1)} />
-    <h1 className={styles.heading}>Review Idea</h1>
-    {page === 'review' ? (
-      <>
-        <div className={styles.detailsContainer}>
-         
-            <div className={styles.details}>
-              <p className={styles.leftText}>
-                <strong>Component Name:</strong>
-                {ideas.name ? <HtmlRenderer htmlString={ideas.name} /> : 'None'}
-              </p>
-              <p className={styles.leftText} id={styles.leftdown}>
-                <strong>Type:</strong>
-                {ideas.type ? <HtmlRenderer htmlString={ideas.type} /> : 'None'}
-              </p>
-              <p className={styles.leftText} id={styles.leftdown}>
-                <strong>Description:</strong>
-                {ideas.description?.full ? <HtmlRenderer htmlString={ideas.description.full} /> : 'None'}
-              </p>
-            </div>
-          
-          <div className={styles.details}>
-            <p>
-              <strong>Details:</strong>
-              <ol className={styles.detaillist} style={{ listStyleType: 'upper-roman' }}>
-                <li className={styles.detailtext}>
-                  <strong>System Requirements:</strong>
-                  {ideas.sys_requirements ? <HtmlRenderer htmlString={ideas.sys_requirements} /> : 'None'}
-                </li>
-                <li className={styles.detailtext}>
-                  <strong>Dependencies:</strong>
-                  {ideas.dependencies ? <HtmlRenderer htmlString={ideas.dependencies} /> : 'None'}
-                </li>
-                <li className={styles.detailtext}>
-                  <strong>License:</strong>
-                  {ideas.license ? <HtmlRenderer htmlString={ideas.license} /> : 'None'}
-                </li>
-              </ol>
-            </p>
-            {/* <div className={styles.downloadContainer}>
-            {/* <a href="#"
-                        onClick={(e) => {
-                          e.preventDefault();
-                          handleDownloadAll(ideas.file);
-                        }}
->
-                <svg xmlns="http://www.w3.org/2000/svg" width="40px" height="40px" viewBox="0 0 24 24" fill="none">
-                  <path d="M12 7L12 14M12 14L15 11M12 14L9 11" stroke="#1C274C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                  <path d="M16 17H12H8" stroke="#1C274C" strokeWidth="1.5" strokeLinecap="round" />
-                  <path d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C21.5093 4.43821 21.8356 5.80655 21.9449 8" stroke="#1C274C" strokeWidth="1.5" strokeLinecap="round" />
-                </svg>
-                Download Files
-              </a> }
-            </div> */}
-          </div>
-        </div>
-        <div className={styles.buttons}>
-          <button type="button" className={styles.cancel} onClick={handleReject}>Reject</button>
-          <button type="button" className={styles.next} onClick={handleAccept}>Accept</button>
-        </div>
-      </>
-    ): (
-      <div className={styles.card}>
-      <div className={styles.cardContent}>
-        <h2>{page === 'reject' ? 'Reject Component' : 'Accept Component'}</h2>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="remarks" className={styles.label}>Remarks:</label>
-          <textarea
-            id="remarks"
-            rows="4"
-            cols="50"
-            value={remarks}
-            onChange={(e) => setRemarks(e.target.value)}
-            className={styles.textarea}
-          ></textarea>
-          {hasSubmitted && errors.remarks && <div style={{ color: 'red' }}>{errors.remarks}</div>}
-
-          <div className={styles.ratingContainer}>
-            <span style={{ fontWeight: 'bold' }}>{tech ? 'Functional Review' : 'Legal Review'}</span>
-            <div>
-              {[...Array(5)].map((star, index) => (
-                <span
-                  key={index}
-                  onClick={() => handleStarClick(index)}
-                  style={{
-                    fontSize: '2em',
-                    cursor: 'pointer',
-                    color: index < rating ? 'gold' : 'gray'
-                  }}
-                >
-                  ★
-                </span>
-              ))}
-            </div>
-            {hasSubmitted && errors.rating && <div style={{ color: 'red' }}>{errors.rating}</div>}
-          </div>
-
-         < div className={styles.buttons}>
-            <button type="button" className={styles.cancel} onClick={() => setPage('review')}>Go Back</button>
-            <button type="submit" className={styles.next} onClick={handleOnClick}>Submit</button>
-            </div>
+    <Box sx={{ padding: 4, width: '80%', margin: '0 auto', minHeight: '100vh', backgroundColor: '#f7f7f7', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+      <IconButton onClick={() => navigate(-1)} sx={{ marginBottom: 2 }}>
+        <RiArrowGoBackFill size={24} />
+      </IconButton>
+      <Typography variant="h4" gutterBottom sx={{ fontSize: '2rem', color: '#333' }}>
+        Review Idea
+      </Typography>
+      {page === 'review' ? (
+        <>
+          <Paper elevation={3} sx={{ padding: 4, marginBottom: 3, minHeight: '500px', backgroundColor: '#fff', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6}>
+                <Typography variant="body1" sx={{ marginBottom: 2, fontSize: '1.25rem' }}>
+                  <strong>Component Name:</strong> {ideas.name ? <HtmlRenderer htmlString={ideas.name} /> : 'None'}
+                </Typography>
+                <Typography variant="body1" sx={{ marginBottom: 2, fontSize: '1.25rem' }}>
+                  <strong>Type:</strong> {ideas.type ? <HtmlRenderer htmlString={ideas.type} /> : 'None'}
+                </Typography>
+                <Typography variant="body1" sx={{ marginBottom: 2, fontSize: '1.25rem' }}>
+                  <strong>Description:</strong> {ideas.description?.full ? <HtmlRenderer htmlString={ideas.description.full} /> : 'None'}
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.25rem' }}>
+                  <strong>Details:</strong>
+                </Typography>
+                <ol style={{ listStyleType: 'upper-roman', paddingLeft: '1.5rem', marginTop: '20px' }}>
+                  <li>
+                    <Typography variant="body2" sx={{ marginTop: '10px', fontSize: '1.25rem' }}>
+                      <strong>System Requirements:</strong> {ideas.sys_requirements ? <HtmlRenderer htmlString={ideas.sys_requirements} /> : 'None'}
+                    </Typography>
+                  </li>
+                  <li>
+                    <Typography variant="body2" sx={{ marginTop: '10px', fontSize: '1.25rem' }}>
+                      <strong>Dependencies:</strong> {ideas.dependencies ? <HtmlRenderer htmlString={ideas.dependencies} /> : 'None'}
+                    </Typography>
+                  </li>
+                  <li>
+                    <Typography variant="body2" sx={{ marginTop: '10px', fontSize: '1.25rem' }}>
+                      <strong>License:</strong> {ideas.license ? <HtmlRenderer htmlString={ideas.license} /> : 'None'}
+                    </Typography>
+                  </li>
+                </ol>
+              </Grid>
+            </Grid>
+          </Paper>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', marginTop: 2 }}>
+            <Button variant="contained" color="error" onClick={handleReject} sx={{ boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+              Reject
+            </Button>
+            <Button variant="contained" color="primary" onClick={handleAccept} sx={{ boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+              Accept
+            </Button>
+          </Box>
+        </>
+      ) : (
+        <Paper elevation={3} sx={{ padding: 4, marginTop: 3, minHeight: '500px', backgroundColor: '#fff', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+          <Typography variant="h5" gutterBottom sx={{ fontSize: '1.5rem', color: '#333' }}>
+            {page === 'reject' ? 'Reject Component' : 'Accept Component'}
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              id="remarks"
+              label="Remarks"
+              multiline
+              rows={4}
+              fullWidth
+              value={remarks}
+              onChange={(e) => setRemarks(e.target.value)}
+              variant="outlined"
+              margin="normal"
+              sx={{ backgroundColor: '#f0f0f0', borderRadius: '5px' }}
+            />
+            <Box sx={{ marginTop: 2 }}>
+              <Typography component="legend" sx={{ fontWeight: 'bold', color: '#333' }}>
+                {userEmail === 'tech' ? 'Functional Review' : 'Legal Review'}
+              </Typography>
+              <Rating
+                name="rating"
+                value={rating}
+                onChange={(event, newValue) => handleStarClick(newValue - 1)}
+              />
+            </Box>
+            <Divider sx={{ marginY: 2 }} />
+            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Button variant="outlined"
+              onClick={() => setPage('review')}
+              sx={{ boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}
+            >
+              Go Back
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              onClick={handleOnClick}
+              sx={{ boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}
+            >
+              Submit
+            </Button>
+          </Box>
         </form>
-      </div>
-    </div>
-  )}
-</div>
-);
+      </Paper>
+      )}
+    </Box>
+  );
 };
+
 export default Review;
